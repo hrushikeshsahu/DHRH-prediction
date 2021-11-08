200ae63fb1754516e4175c3d68d5ce09396a1117
hrushikeshsahu <93906609+hrushikeshsahu@users.noreply.github.com>
2021-11-08 15:18:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+pca = pickle.load(open('pca.pkl', 'rb'))
+scaler = pickle.load(open('scaler.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [int(x) for x in request.form.values()]
+    scaler_features= scaler.transform(np.array(int_features).reshape(1, -1))
+    pca_features=pca.transform(scaler_features)
+    prediction = model.predict(pca_features)
+    final_prediction=prediction**2
+
+    output = round(final_prediction[0], 2)
+
+    return render_template('index.html', final_prediction='DGRH should be {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
